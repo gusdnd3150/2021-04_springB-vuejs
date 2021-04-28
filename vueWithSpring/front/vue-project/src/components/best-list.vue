23405cf8dc186ab6d7dc46d76af9d850bc677ffe
khw <gusdnd3150@gmail.com>
2021-04-28 17:52:29 +0900

    베스트 리스트 컴포넌트 추가

---
+++ vueWithSpring/front/vue-project/src/components/best-list.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'best-list',
+  data () {
+    return {
+      name: 'dd'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- vueWithSpring/front/vue-project/src/view/login.vue
+++ vueWithSpring/front/vue-project/src/view/login.vue
@@ -118,7 +118,7 @@ export default {
   width: 800px;
   margin: 0 auto;
 }
-@media (max-width: 767px){
+@media (max-width: 750px){
   .content-area{
     width:100%;
   }
