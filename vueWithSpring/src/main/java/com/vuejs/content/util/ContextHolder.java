d1f6b4b50c399e8c2396e9265ad27028cf25f44f
HyeonWoongKang <gusdnd3150@gmail.com>
2021-06-01 14:17:15 +0900

    ContextHodler 유틸 추가

---
+++ vueWithSpring/src/main/java/com/vuejs/content/util/ContextHolder.java
@@ -0,0 +1,21 @@
+package com.vuejs.content.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContextHolder  implements ApplicationContextAware  {
+
+	private static ApplicationContext appCtx;
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		appCtx = applicationContext; 
+	}
+	
+	public static ApplicationContext getContext() {
+		return appCtx;
+	}
+}
